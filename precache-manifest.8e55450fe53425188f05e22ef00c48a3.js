7ede260fb96b959a6ad3163aae2cde7ba8e1b945
amanporwal <amanporwal510@gmail.com>
2021-05-16 02:33:52 +0530

    Updates

---
+++ precache-manifest.8e55450fe53425188f05e22ef00c48a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f498ca0e48d0b4d435",
+    "url": "/colorFarm/static/css/main.a8130f8c.chunk.css"
+  },
+  {
+    "revision": "38f498ca0e48d0b4d435",
+    "url": "/colorFarm/static/js/main.1ef4aa22.chunk.js"
+  },
+  {
+    "revision": "99a7f37ef14b4e347822",
+    "url": "/colorFarm/static/js/runtime~main.0fe51c89.js"
+  },
+  {
+    "revision": "8af44312642b0c8a3e53",
+    "url": "/colorFarm/static/css/2.96f07925.chunk.css"
+  },
+  {
+    "revision": "8af44312642b0c8a3e53",
+    "url": "/colorFarm/static/js/2.f91473bf.chunk.js"
+  },
+  {
+    "revision": "f8512115782175dce09b",
+    "url": "/colorFarm/static/js/3.b6bf17c7.chunk.js"
+  },
+  {
+    "revision": "ddf7fec24f7cf5d3189a7c7482be0c15",
+    "url": "/colorFarm/static/media/bg.ddf7fec2.svg"
+  },
+  {
+    "revision": "dc4c852ff7cec076231ce16ed48d8c5d",
+    "url": "/colorFarm/index.html"
+  }
+];
