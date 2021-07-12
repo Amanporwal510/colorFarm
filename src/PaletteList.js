1b167daba6407b758269700d0e3355172cb37229
Aman Porwal <amanporwal510@gmail.com>
2021-07-12 17:31:49 +0530

    changed name in palleteList.js (React Color -> ColorFarm)

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -43,7 +43,7 @@ class PaletteList extends Component {
             <div className={classes.root}>
                 <div className={classes.container}>
                     <nav className={classes.nav}>
-                        <h1 className={classes.heading} >React Colors</h1>
+                        <h1 className={classes.heading} >ColorFarm</h1>
                         <Link to='/palette/new'>Create Palette</Link>
                     </nav>
                     <TransitionGroup className={classes.palettes}>
